7904a067a2eb9ac891d43822cb133fcb61dcfebe
sh90 <shikhatyagi1990@gmail.com>
2025-05-31 21:01:23 +0530

    Create 2.chunking.py

---
+++ 2.chunking.py
@@ -0,0 +1,14 @@
+import os
+from langchain.document_loaders import TextLoader, PyPDFLoader, UnstructuredWordDocumentLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+file_path = "data/spgi-annual-report-2023.pdf"
+loader = PyPDFLoader(file_path)
+docs1 = loader.load()
+
+print("Done")
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=450)
+docs = splitter.split_documents(docs1)
+
+print("Done")
